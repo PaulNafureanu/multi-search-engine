e50656710fd032ccd302709ed654c31b8d2fe762
Paul Nafureanu <paul.nafureanu@gmail.com>
2024-05-26 11:21:35 +0300

    Init commit

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("node:path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+exports.entry = "./src/index.ts";
+exports.module = {
+  rules: [
+    {
+      test: /\.(ts|tsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: "ts-loader",
+      },
+    },
+    {
+      test: /\.css$/,
+      exclude: /node_modules/,
+      use: ["style-loader", "css-loader"],
+    },
+  ],
+};
+exports.resolve = {
+  extensions: [".tsx", ".ts", ".js", "jsx"],
+};
+exports.output = {
+  filename: "bundle.js",
+  path: path.resolve(__dirname, "dist"),
+};
+
+exports.plugins = [
+  new HtmlWebpackPlugin({
+    title: "Multi search engine",
+    meta: { viewport: "width=device-width, initial-scale=1, shrink-to-fit=no" },
+  }),
+];
+exports.devServer = {
+  static: {
+    directory: path.join(__dirname, "public"),
+  },
+  compress: true,
+  port: 9000,
+};
